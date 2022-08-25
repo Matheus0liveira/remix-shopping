f800b775ded7ff60870214e60529b1800c668509
Matheus Oliveira <contato@moliveira.dev>
2022-08-25 13:45:57 -0300

    feat: add input when editable user profile

--- app/features/users/components/UserDetailItem.tsx
+++ app/features/users/components/UserDetailItem.tsx
@@ -1,14 +1,50 @@
+import type { HTMLInputTypeAttribute } from "react";
+
 export type UserDetailItemProps = {
+  label: string;
   name: string;
   value: string;
+  editable?: boolean;
+  type?: HTMLInputTypeAttribute;
 };
 
-export function UserDetailItem({ name, value }: UserDetailItemProps) {
+export function UserDetailItem({
+  type = "text",
+  label,
+  name,
+  value,
+  editable = true,
+}: UserDetailItemProps) {
   return (
     <div className="bg-white border-b border-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-      <dt className="text-sm font-medium text-gray-500">{name}</dt>
+      <dt className="text-sm font-medium text-gray-500">
+        {!editable ? (
+          label
+        ) : (
+          <label
+            htmlFor={name}
+            className="block text-sm font-medium text-gray-700"
+          >
+            {label}
+          </label>
+        )}
+      </dt>
       <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between items-center">
-        {value}
+        {editable ? (
+          <div>
+            <div className="mt-1 relative rounded-md shadow-sm">
+              <input
+                type={type}
+                name={name}
+                id={name}
+                defaultValue={value}
+                className="focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2 sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+          </div>
+        ) : (
+          value
+        )}
       </dd>
     </div>
   );
--- app/features/users/components/UserDetails.tsx
+++ app/features/users/components/UserDetails.tsx
@@ -1,37 +1,67 @@
+import { useState } from "react";
+import { classNames } from "~/shared";
 import { UserDetailItem } from "./UserDetailItem";
 
 export function UserDetails() {
+  const [isEditable, setIsEditable] = useState(false);
   return (
     <div className="max-w-7xl mx-auto px-2 mt-8">
       <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-        <div className="px-4 py-5 sm:px-6 flex justify-between items-center">
-          <div>
-            <h3 className="text-lg leading-6 font-medium text-gray-900">
-              User Info
-            </h3>
-            <p className="mt-1 max-w-2xl text-sm text-gray-500">
-              Personal details.
-            </p>
+        <form className="border-t border-gray-200">
+          <div className="px-4 py-5 sm:px-6 flex justify-between items-center">
+            <div>
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                User Info
+              </h3>
+              <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                Personal details.
+              </p>
+            </div>
+            <div className="flex gap-2">
+              <button
+                type="button"
+                className={classNames(
+                  "border rounded-md py-1 px-8 flex items-center justify-center text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 ",
+                  isEditable
+                    ? "bg-gray-500 hover:bg-neutral-400 focus:ring-neutral-500"
+                    : "bg-indigo-600 border-indigo-600 border-transparent hover:bg-indigo-700 focus:ring-indigo-500"
+                )}
+                onClick={() => setIsEditable((pv) => !pv)}
+              >
+                {isEditable ? "Cancelar" : "Editar"}
+              </button>
+              {isEditable && (
+                <button
+                  type="submit"
+                  className="bg-indigo-600 border-indigo-600 border-transparent hover:bg-indigo-700 focus:ring-indigo-500 border rounded-md py-1 px-8 flex items-center justify-center text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2"
+                >
+                  Salvar
+                </button>
+              )}
+            </div>
           </div>
-          <div>
-            <button
-              type="button"
-              className=" bg-indigo-600 border border-transparent rounded-md py-1 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Editar
-            </button>
-          </div>
-        </div>
-        <div className="border-t border-gray-200">
           <dl>
-            <UserDetailItem name="Full name" value="Margot Foster" />
             <UserDetailItem
-              name="Email address"
-              value="margotfoster@example.com"
+              label="Full name"
+              name="full_name"
+              value="Margot Foster"
+              editable={isEditable}
             />
-            <UserDetailItem name="Password" value="******" />
+            <UserDetailItem
+              label="Email address"
+              name="email_address"
+              value="margotfoster@example.com"
+              editable={isEditable}
+            />
+            <UserDetailItem
+              type="password"
+              label="Password"
+              name="password"
+              value="******"
+              editable={isEditable}
+            />
           </dl>
-        </div>
+        </form>
       </div>
     </div>
   );
