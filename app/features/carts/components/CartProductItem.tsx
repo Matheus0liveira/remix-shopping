b4d9fc2ca9169dcc20dc775cf2e23a744fd44f87
Matheus Oliveira <contato@moliveira.dev>
2022-08-27 11:33:15 -0300

    feat: create CartProductItem component

--- app/features/carts/components/CartProductItem.tsx
+++ app/features/carts/components/CartProductItem.tsx
@@ -1,24 +1,21 @@
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
+import { formatNumberToCurrencyBRL } from "~/utils/currency";
+import type { Type } from "..";
 
 export type CartProductItemProps = {
-  product: {
-    id: number;
-    name: string;
-    price: string;
-    href: string;
-    imageSrc: string;
-    imageAlt: string;
-    quantity: number;
-  };
+  product: Type.Carts.ProductWithQuantity;
 };
 
 export function CartProductItem({ product }: CartProductItemProps) {
+  const formatedTotalPrice = formatNumberToCurrencyBRL(product.totalPrice);
+  const formatedPromoPrice =
+    product.promoPrice && formatNumberToCurrencyBRL(product.promoPrice);
   return (
     <li className="flex py-6">
       <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
         <img
           src={product.imageSrc}
-          alt={product.imageAlt}
+          alt={product.name}
           className="h-full w-full object-cover object-center"
         />
       </div>
@@ -27,22 +24,26 @@ export function CartProductItem({ product }: CartProductItemProps) {
         <div>
           <div className="flex justify-between text-base font-medium text-gray-900">
             <h3>
-              <Link to={product.href}> {product.name} </Link>
+              <Link to={product.imageSrc}> {product.name} </Link>
             </h3>
-            <p className="ml-4">{product.price}</p>
+            <p className="ml-4">{formatedPromoPrice ?? formatedTotalPrice}</p>
           </div>
         </div>
         <div className="flex flex-1 items-end justify-between text-sm">
           <p className="text-gray-500">Qty {product.quantity}</p>
 
-          <div className="flex">
+          <Form
+            className="flex"
+            action={`/products/${product.id}/cart/delete`}
+            method="post"
+          >
             <button
-              type="button"
+              type="submit"
               className="font-medium text-indigo-600 hover:text-indigo-500"
             >
               Remove
             </button>
-          </div>
+          </Form>
         </div>
       </div>
     </li>
