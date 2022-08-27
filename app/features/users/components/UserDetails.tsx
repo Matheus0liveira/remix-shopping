d2b8b2266e27d3861620e447c850df7754b715c8
Matheus Oliveira <contato@moliveira.dev>
2022-08-27 17:38:15 -0300

    chore: add db to git

--- app/features/users/components/UserDetails.tsx
+++ app/features/users/components/UserDetails.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import { classNames } from "~/shared";
 import { UserDetailItem } from "./UserDetailItem";
 
 export function UserDetails() {
-  const [isEditable, setIsEditable] = useState(false);
   return (
     <div className="max-w-7xl mx-auto px-2 mt-8">
       <div className="bg-white shadow overflow-hidden sm:rounded-lg">
@@ -17,49 +14,14 @@ export function UserDetails() {
                 Personal details.
               </p>
             </div>
-            <div className="flex gap-2">
-              <button
-                type="button"
-                className={classNames(
-                  "border rounded-md py-1 px-8 flex items-center justify-center text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 ",
-                  isEditable
-                    ? "bg-gray-500 hover:bg-neutral-400 focus:ring-neutral-500"
-                    : "bg-indigo-600 border-indigo-600 border-transparent hover:bg-indigo-700 focus:ring-indigo-500"
-                )}
-                onClick={() => setIsEditable((pv) => !pv)}
-              >
-                {isEditable ? "Cancelar" : "Editar"}
-              </button>
-              {isEditable && (
-                <button
-                  type="submit"
-                  className="bg-indigo-600 border-indigo-600 border-transparent hover:bg-indigo-700 focus:ring-indigo-500 border rounded-md py-1 px-8 flex items-center justify-center text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2"
-                >
-                  Salvar
-                </button>
-              )}
-            </div>
           </div>
           <dl>
-            <UserDetailItem
-              label="Full name"
-              name="full_name"
-              value="Margot Foster"
-              editable={isEditable}
-            />
+            <UserDetailItem label="Full name" value="Margot Foster" />
             <UserDetailItem
               label="Email address"
-              name="email_address"
               value="margotfoster@example.com"
-              editable={isEditable}
             />
-            <UserDetailItem
-              type="password"
-              label="Password"
-              name="password"
-              value="******"
-              editable={isEditable}
-            />
+            <UserDetailItem label="Password" value="******" />
           </dl>
         </form>
       </div>
--- app/features/users/utils/cookies.server.ts
+++ app/features/users/utils/cookies.server.ts
@@ -1,3 +1,22 @@
 import { createCookie } from "@remix-run/node";
+import { decode } from "jsonwebtoken";
+import type { Type } from "..";
 
 export const cookieUserToken = createCookie("user-token");
+
+export const getToken = async (request: Request): Promise<string> => {
+  const cookieHeader = request.headers.get("Cookie");
+  const { token } = await cookieUserToken.parse(cookieHeader);
+
+  return token;
+};
+
+export const getUser = async (
+  request: Request
+): Promise<Type.Users.JWTUser> => {
+  const token = await getToken(request);
+
+  const user = decode(token) as Type.Users.JWTUser;
+
+  return user;
+};
